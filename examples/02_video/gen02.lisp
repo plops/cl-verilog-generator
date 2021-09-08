b6f80d994471cae73dcd9b62506b0e6c78539fd8
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-09-08 20:11:36 +0200

    start encoding the register table

--- examples/02_video/gen02.lisp
+++ examples/02_video/gen02.lisp
@@ -4,3 +4,11 @@
 (in-package :cl-ov2640)
 
 ;; register parser for ov2640 camera
+
+(let
+    ((l
+       `((0 rsvd)
+	 (5 r-bypass #x1 rw (((7 1) rsvd)
+			     ((0) bypass-dsp-select ((0 dsp) (1 bypass)))))
+	 (6 rsvd)
+	 (44 qs #x0c rw quantization-scale-factor)))))
