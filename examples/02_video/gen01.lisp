0843539274591bb4b275f767fd065368904ac132
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-09-15 22:37:10 +0200

    look at i2c again

--- examples/02_video/gen01.lisp
+++ examples/02_video/gen01.lisp
@@ -205,94 +205,103 @@
     
     (write-source
      (format nil "~a/source/i2c_interface.v" *path*)
-     `(module i2c_interface
-	      ("input clk"		;; 50MHz
-	       "inout siod"		;; SCCB data signal
-	       "output sioc"		;; SCCB clock signal
-	       "output taken" ;; flag to go to next address of LUT
-	       "input send" ;; flag to indicate if configuration has finished
-	       "input [7:0] rega" ;; register address
-	       "input [7:0] value" ;; data to write into register address
-	       )
-	      ,@(loop for e in `((divider 7 "8'b00000001")
-				 (busy_sr 31 "{32{1'b0}}")
-				 (data_sr 31 "{32{1'b1}}")
-				 (sioc_temp)
-				 (taken_temp)
-				 (siod_temp))
-		      collect
-		      (destructuring-bind (name &optional size default) e
-			(format nil "reg ~@[[~a:0]~] ~a~@[ =~a~];" size name default)))
-	      ,@(loop for e in `(siod sioc taken)
-		      collect
-		      `(assign ,e ,(format nil "~a_temp" e)))
-	      (always-at (or busy_sr (aref data_sr 31))
-			 ;; tristate when idle or siod driven by master
-			 (if (logior
-			      (== (aref busy_sr (slice 11 10))
-				  "2'b10")
-			      (== (aref busy_sr (slice 20 19))
-				  "2'b10")
-			      (== (aref busy_sr (slice 29 28))
-				  "2'b10"))
-			     (setf siod_temp "1'bZ")
-			     (setf siod_temp (aref data_sr 31))))
-	      (always-at
-	       "posedge clk"
-	       (setf taken_temp "1'b0")
-	       (if (== (aref busy_sr 31)
-		       0)
-		   (do0
-		    (setf sioc_temp 1)
-		    (if (== send 1)
-			(if (== divider "8'b0000_0000")
-			    (setf data_sr (concat "3'b100"
-						  "8'h42" ; id
-						  "1'b0"
-						  rega
-						  "1'b0"
-						  value
-						  "1'b0"
-						  "2'b01")
-				  busy_sr (concat "3'b111"
-						  "9'b1_1111_1111"
-						  "9'b1_1111_1111"
-						  "9'b1_1111_1111"
-						  "2'b11")
-				  taken_temp "1'b1")
-			    (incf divider))
+     `(do0
+       "///////"
+       "//"
+       "//"
+       "//"
+       "//"
+       "///////"
+       (module i2c_interface
+	       ("input clk"   ;; 50MHz
+		"inout siod"  ;; SCCB data signal
+		"output sioc" ;; SCCB clock signal
+		"output taken" ;; flag to go to next address of LUT
+		"input send" ;; flag to indicate if configuration has finished
+		"input [7:0] rega" ;; register address
+		"input [7:0] value" ;; data to write into register address
+		)
+	       ,@(loop for e in `((divider 7 "8'b00000001")
+				  (busy_sr 31 "{32{1'b0}}")
+				  (data_sr 31 "{32{1'b1}}")
+				  (sioc_temp)
+				  (taken_temp)
+				  (siod_temp))
+		       collect
+		       (destructuring-bind (name &optional size default) e
+			 (format nil "reg ~@[[~a:0]~] ~a~@[ =~a~];" size name default)))
+	       "// ID of an OV7670 for SCCB protocol"
+	       "localparam id = 8'h42;"
+	       ,@(loop for e in `(siod sioc taken)
+		       collect
+		       `(assign ,e ,(format nil "~a_temp" e)))
+	       (always-at (or busy_sr (aref data_sr 31))
+			  ;; tristate when idle or siod driven by master
+			  (if (logior
+			       (== (aref busy_sr (slice 11 10))
+				   "2'b10")
+			       (== (aref busy_sr (slice 20 19))
+				   "2'b10")
+			       (== (aref busy_sr (slice 29 28))
+				   "2'b10"))
+			      (setf siod_temp "1'bZ")
+			      (setf siod_temp (aref data_sr 31))))
+	       (always-at
+		"posedge clk"
+		(setf taken_temp "1'b0")
+		(if (== (aref busy_sr 31)
+			0)
+		    (do0
+		     (setf sioc_temp 1)
+		     (if (== send 1)
+			 (if (== divider "8'b0000_0000")
+			     (setf data_sr (concat "3'b100"
+						   id
+						   "1'b0"
+						   rega
+						   "1'b0"
+						   value
+						   "1'b0"
+						   "2'b01")
+				   busy_sr (concat "3'b111"
+						   "9'b1_1111_1111"
+						   "9'b1_1111_1111"
+						   "9'b1_1111_1111"
+						   "2'b11")
+				   taken_temp "1'b1")
+			     (incf divider))
 			
 			
-			(do0
-			 (case (concat (aref busy_sr (slice 31 29))
-				       (aref busy_sr (slice 2 0)))
-			   ,@(loop for e in `(("6'b111_111" 1 1 1 1)
-					      ("6'b111_110" 1 1 1 1)
-					      ("6'b111_100" 0 0 0 0)
-					      ("6'b110_000" 0 1 1 1)
-					      ("6'b100_000" 1 1 1 1)
-					      ("6'b000_000" 1 1 1 1)
-					      (t 0 1 1 0)
-					      )
+			 (do0
+			  (case (concat (aref busy_sr (slice 31 29))
+					(aref busy_sr (slice 2 0)))
+			    ,@(loop for e in `(("6'b111_111" 1 1 1 1)
+					       ("6'b111_110" 1 1 1 1)
+					       ("6'b111_100" 0 0 0 0)
+					       ("6'b110_000" 0 1 1 1)
+					       ("6'b100_000" 1 1 1 1)
+					       ("6'b000_000" 1 1 1 1)
+					       (t 0 1 1 0)
+					       )
 				   
-				   collect
-				   (destructuring-bind (top-key a b c d) e
-				     `(,top-key
-				       (case (aref divider (slice 7 6))
-					 ,@(loop for key in `("2'b00" "2'b01" "2'b10" t)
-						 and f in (list a b c d)
-						 collect
-						 `(,key (setf sioc_temp ,f))))))))
+				    collect
+				    (destructuring-bind (top-key a b c d) e
+				      `(,top-key
+					(case (aref divider (slice 7 6))
+					  ,@(loop for key in `("2'b00" "2'b01" "2'b10" t)
+						  and f in (list a b c d)
+						  collect
+						  `(,key (setf sioc_temp ,f))))))))
 
-			 
-			 (if (== divider "8'b1111_1111")
-			     (setf busy_sr (concat (aref busy_sr (slice 30 0))
-						   "1'b0")
-				   data_sr (concat (aref data_sr (slice 30 0))
-						   "1'b1")
-				   divider "{8{1'b0}}"
-				   )
-			     (incf divider))))))))))
+			 "// create 200kHz frequency for sccb"
+			  (if (== divider "8'b1111_1111")
+			      (setf busy_sr (concat (aref busy_sr (slice 30 0))
+						    "1'b0")
+				    data_sr (concat (aref data_sr (slice 30 0))
+						    "1'b1")
+				    divider "{8{1'b0}}"
+				    )
+			      (incf divider)))))))))))
   (write-source
    (format nil "~a/source/syn_gen.v" *path*)
    `(module syn_gen
